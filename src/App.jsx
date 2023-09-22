48f17f394d27d0b1482bc34714110176afb39063
Pyrashima <63759801+MyriamNguyen@users.noreply.github.com>
2023-09-22 00:30:42 -0400

    Remise exercice formulaire 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Container from "react-bootstrap/Container";
+import LoginForm from "./components/Form";
+
+function App() {
+  return (
+    <Container>
+      <LoginForm />
+    </Container>
+  );
+}
+
+export default App;
